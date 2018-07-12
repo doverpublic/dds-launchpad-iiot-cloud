2b185e221da725b50845b5793a60717951ceb75c
Naor <naor@lancastersoftware.com>
2018-07-11 21:16:39 -0400

    Adding api's in the TargetSolutionUI application.

--- src/TargetSolutionUI/routes/index.js
+++ src/TargetSolutionUI/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 const launchpadRoute = require('./launchpad-api');
-const keymoduleRoute = require('./keymodule-api');
+const moduleRoute = require('./module-api');
 
 app.get('/', (req, res) => {
         res.json({ item: 'Dover Launchpad iIoT RESTful API - Version 1.0.0' });
     })
     .use('/help', launchpadRoute)
-    .use('/keymodule', keymoduleRoute);
+    .use('/module', moduleRoute);
 
 module.exports = app;
