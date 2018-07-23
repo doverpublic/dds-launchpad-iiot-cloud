6bfa724e57816d1410a6b22c3ff9cdaa0fdab089
Naor <naor@lancastersoftware.com>
2018-07-23 16:19:12 -0400

    Updated the repository with latest changes.

---
+++ src/TargetSolutionUI/src/assets/common/_ref/util/hashutil.js
@@ -0,0 +1,20 @@
+// Launchpad Common Libraries
+const uuidv4 = require('uuid/v4')
+
+
+var getUniqueId = function() {
+    var strRet = "";
+    var uuidValue = uuidv4();
+    var parts = uuidValue.split("-");
+
+    for (var index = 0; index < parts.length; index++) {
+        strRet += parts[index];
+    }
+}
+
+return strRet;
+}
+
+module.exports = {
+    getUniqueId: getUniqueId
+};
