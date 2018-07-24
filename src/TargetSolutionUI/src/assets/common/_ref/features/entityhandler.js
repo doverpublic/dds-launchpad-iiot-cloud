85aa0d6c6103dd111c56f4f1738ec7a4bca5d6bb
Naor <naor@lancastersoftware.com>
2018-07-24 04:32:21 -0400

    First pass on the Entities API

---
+++ src/TargetSolutionUI/src/assets/common/_ref/features/entityhandler.js
@@ -0,0 +1,81 @@
+// Launchpad Common Libraries
+var Db = require('tingodb')().Db,
+    assert = require('assert');
+
+var localEntityDBPath = './data/db/entities';
+
+var entitiesDb = new Db(localEntityDBPath, {});
+
+var deleteOne = function(entityType, entityId) {
+    var collection = entitiesDb.collection(entityType);
+    var entityQuery = '{ "id": "' + entityId + '"}';
+
+    collection.remove(JSON.parse(entityQuery), { w: 1 }, function(err, result) {
+        assert.equal(null, err);
+        assert.equal(1, result);
+    });
+};
+
+var insertOne = function(entityType, entityJsonObj) {
+    var collection = entitiesDb.collection(entityType);
+    var jsonArray = '[' + JSON.stringify(entityJsonObj) + ']';
+
+    collection.insert(JSON.parse(jsonArray), { w: 1 }, function(err, result) {
+        assert.equal(null, err);
+    });
+};
+
+var readOne = function(entityType, entityId, callback) {
+    var defaultRet = JSON.parse("{}");
+    var collection = entitiesDb.collection(entityType);
+    var entityQuery = '{ "id": "' + entityId + '"}';
+
+    console.log("On entityhandler.readOne - Entity Query[%s]", entityQuery);
+
+    queryCollection(collection, entityQuery, defaultRet, true, callback);
+};
+
+var readAll = function(entityType, callback) {
+    var defaultRet = JSON.parse("[{}]");
+    var collection = entitiesDb.collection(entityType);
+    var entityQuery = '{}';
+
+    queryCollection(collection, entityQuery, defaultRet, false, callback);
+};
+
+var updateOne = function(entityType, entityId, entityJsonObj) {
+    var collection = entitiesDb.collection(entityType);
+    var entityQuery = '{ "id": "' + entityId + '"}';
+
+    collection.update(JSON.parse(entityQuery), entityJsonObj, { upsert: true, w: 1 }, function(err, result) {
+        assert.equal(null, err);
+        assert.equal(1, result);
+    });
+};
+
+
+// Local Methods
+
+function queryCollection(collection, query, defaultReturn, readOneFlag, callback) {
+    collection.find(JSON.parse(query)).toArray(function(err, result) {
+        if (err) {
+            console.log(err);
+        } else if (result.length > 0) {
+            if (readOneFlag)
+                callback(result[0]);
+            else
+                callback(result);
+        } else {
+            callback(defaultReturn);
+        }
+    });
+}
+
+
+module.exports = {
+    deleteOne: deleteOne,
+    insertOne: insertOne,
+    readOne: readOne,
+    readAll: readAll,
+    updateOne: updateOne
+};
--- src/TargetSolutionUI/src/assets/controllers/_ref/entity-api.js
+++ src/TargetSolutionUI/src/assets/controllers/_ref/entity-api.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 var swaggerJSDoc = require('swagger-jsdoc');
 var hashutil = require('../../common/_ref/util/hashutil.js');
+var entityhandler = require('../../common/_ref/features/entityhandler.js');
 var apiDefPath = './dist/TargetSolutionUI/assets/controllers/_ref/entity-api.js';
 var apiDocPath = './dist/TargetSolutionUI/assets/controllers/_ref/entity-doc.json';
+
+app.use(bodyParser.json());
 
 var options = {
     swaggerDefinition: {
@@ -120,5 +124,163 @@ app.get('/:class/template', (req, res) => {
     readable.pipe(res);
 });
 
+/**
+ * @swagger
+ * /api/entity/{class}/id/{id}:
+ *   get:
+ *     description: Read the entity instance from the database for a given id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: class
+ *         description: Name of the instance type e.g. User, Company, etc.
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: id
+ *         description: Unique id of the instance assigned as part of the new entity request
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 200 203 response
+ */
+app.get('/:class/id/:id', (req, res) => {
+    var className = req.params.class;
+    var entityId = req.params.id;
+
+    console.log("On entity-api /class/id Entity Type [%s] Id [%s]", className, entityId);
+
+    entityhandler.readOne(className, entityId, function(result) {
+        res.json(result);
+    });
+});
+
+/**
+ * @swagger
+ * /api/entity/{class}/all:
+ *   get:
+ *     description: Read the entities instances from the database for a given entity type
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: class
+ *         description: Name of the instance type e.g. User, Company, etc.
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 200 203 response
+ */
+app.get('/:class/all', (req, res) => {
+    var className = req.params.class;
+
+    entityhandler.readAll(className, function(result) {
+        res.json(result);
+    });
+});
+
+/**
+ * @swagger
+ * /api/entity/{class}/{id}:
+ *   delete:
+ *     description: Delete the entity instance from the database for a given id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: class
+ *         description: Name of the instance type e.g. User, Company, etc.
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: id
+ *         description: Unique id of the instance assigned as part of the new entity request
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 200 203 response
+ */
+app.delete('/:class/:id', (req, res) => {
+    var className = req.params.class;
+    var entityId = req.params.id;
+
+    entityhandler.deleteOne(className, entityId);
+
+    console.log("On entity-api DELETE /class/id Entity Type [%s] Id [%s]", className, id);
+    res.json({ "result": "true" });
+});
+
+
+/**
+ * @swagger
+ * /api/entity/{class}:
+ *   post:
+ *     description: Create a new entity instance in the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: class
+ *         description: Name of the instance type e.g. User, Company, etc.
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: entityObject
+ *         description: Instance oject in JSON format
+ *         in: body
+ *         type: string
+ *         required: true 
+ *     responses:
+ *       200:
+ *         description: 200 203 response
+ */
+app.post('/:class', (req, res) => {
+    var className = req.params.class;
+    var entityJsonObj = req.body;
+    var entityId = entityJsonObj.id;
+
+    console.log("On entity-api CREATE /class/id Entity Type [%s] Id [%s] Obj[%s]", className, entityId, JSON.stringify(entityJsonObj));
+
+    entityhandler.insertOne(className, entityJsonObj);
+
+    res.json({ "result": "true" });
+});
+
+/**
+ * @swagger
+ * /api/entity/{class}:
+ *   put:
+ *     description: Update an existing entity instance in the database
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: class
+ *         description: Name of the instance type e.g. User, Company, etc.
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: instaceObj
+ *         description: Instance oject in JSON format
+ *         in: body
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 200 203 response
+ */
+app.put('/:class', (req, res) => {
+    var className = req.params.class;
+    var entityJsonObj = req.body;
+    var entityId = entityJsonObj.id;
+
+    console.log("On entity-api UPDATE /class/id Entity Type [%s] Id [%s] Obj[%s]", className, entityId, JSON.stringify(entityJsonObj));
+
+    entityhandler.updateOne(className, entityId, entityJsonObj);
+
+    res.json({ "result": "true" });
+});
 
 module.exports = app;
